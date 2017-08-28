d7c20682994595b603374dd353ef93d05ee092dc
JavAlisson <javalisson@gmail.com>
2017-08-28 14:53:22 -0300

    Feita a primeira linha do tabuleiro.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // chamada no inicio do programa
 function setup() {
 	// cria o quadro, com dimensoes 900 x 400
-	createCanvas(900, 400);
+	createCanvas(600, 600);
 }
 
 // chamada toda vez que o quadro for redesenhado
@@ -16,4 +16,28 @@ function draw() {
 	// pinta o fundo de preto
 	background(0, 0, 0);
 
+	// desenha o 1o quadrado preto
+	fill(0, 0, 0);
+	rect(0, 0, 75, 75);
+	// desenha o 1o quadrado branco
+	fill(255, 255, 255);
+	rect(75, 0, 75, 75);
+	// desenha o 2o quadrado preto
+	fill(0, 0, 0);
+	rect(150, 0, 75, 75);
+	// desenha o 2o quadrado branco
+	fill(255, 255, 255);
+	rect(225, 0, 75, 75);
+	// desenha o 3o quadrado preto
+	fill(0, 0, 0);
+	rect(300, 0, 75, 75);
+	// desenha o 3o quadrado branco
+	fill(255, 255, 255);
+	rect(375, 0, 75, 75);
+	// desenha o 4o quadrado preto
+	fill(0, 0, 0);
+	rect(450, 0, 75, 75);
+	// desenha o 4o quadrado branco
+	fill(255, 255, 255);
+	rect(525, 0, 75, 75);
 }
